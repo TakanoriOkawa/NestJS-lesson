efd669edaf7aefec484a1f7e70725a780d08644c
Taka <okawatakanori9916@gmail.com>
2022-12-14 23:52:37 +0900

    ルーティングを試す

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -3,6 +3,11 @@ import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
+  // ルーティングテスト
+  @Get('hello')
+  greeting() {
+    return 'hello';
+  }
   constructor(private readonly appService: AppService) {}
 
   @Get()
