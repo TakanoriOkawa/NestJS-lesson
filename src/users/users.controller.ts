6a0143c7565bf888f562771a9fd579d61644719c
Taka <okawatakanori9916@gmail.com>
2022-12-16 23:46:12 +0900

    クエリパラメータを取得するGETメソッドAPIを実装

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 
 @Controller('users')
@@ -7,5 +7,11 @@ export class UsersController {
   create(@Body() createUser: CreateUserDto) {
     console.log(createUser);
     return createUser;
+  }
+  // ユーザーIDをパスパラメーターから取得
+  @Get(':id')
+  getId(@Param('id') id: number) {
+    console.log(`ユーザーID ${id}を表示`);
+    return id;
   }
 }
